507c0eb333c6e95e82fb13042901366b7e39d8e1
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-31 14:22:07 -0600

    add unit test for #my_each when no block is given

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,1 +1,12 @@
 require_relative '../lib/enumerables.rb'
+
+describe Enumerable do
+  describe '#my_each' do
+    let(:arr) { %w(john david peter) }
+
+    it 'returns enumerator if no block is given' do
+      result = arr.my_each
+      expect(result).to be_instance_of(Enumerator)
+    end
+  end
+end
