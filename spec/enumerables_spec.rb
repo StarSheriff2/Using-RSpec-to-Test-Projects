78570f9c46f749c90992ca7dcd2bd07b6dbb52a3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-31 16:05:43 -0600

    add unit tests for my_each

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,12 +1,42 @@
 require_relative '../lib/enumerables.rb'
 
 describe Enumerable do
-  describe '#my_each' do
-    let(:arr) { %w(john david peter) }
+  let(:strings) { %w(john david peter) }
+  let(:numbers) { [1, 2, 3] }
 
-    it 'returns enumerator if no block is given' do
-      result = arr.my_each
-      expect(result).to be_instance_of(Enumerator)
+  describe '#my_each' do
+    context 'when no block is given' do
+      it 'returns enumerator' do
+        result = strings.my_each
+        expect(result).to be_instance_of(Enumerator)
+      end
+    end
+
+    context 'when a block is given' do
+      it 'returns an array' do
+        result = strings.my_each { |item| item }
+        expect(result).to be_instance_of(Array)
+      end
+
+      it 'calls the given block once for each element and returns array itself' do
+        result = numbers.my_each { |number| number * 2 }
+        expect(result).to eq([1, 2, 3])
+      end
+
+      it 'calls the given block once for each element, passing that element as a parameter' do
+        arr = []
+        numbers.my_each { |number| arr << number * 2 }
+        expect(arr).to eq([2, 4, 6])
+      end
+    end
+  end
+
+  describe '#my_each_with_index' do
+    context 'when no block is given' do
+      it 'returns enumerator' do
+        result = strings.my_each_with_index
+        expect(result).to be_instance_of(Enumerator)
+      end
     end
   end
 end
